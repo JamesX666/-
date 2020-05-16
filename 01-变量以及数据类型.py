31f426d3ec6637da23bf546ac5b3b7810789f55c
JamesX666 <65160106+JamesX666@users.noreply.github.com>
2020-05-16 23:58:56 +0800

    Add files via upload
    5.16

---
+++ 01-变量以及数据类型.py
@@ -0,0 +1,44 @@
+# a 使用一个名字，代替一段内容
+a = '你好世界'
+print(a)
+
+b = 34
+print(b)
+
+# c = good
+# print(c)
+
+d = True
+print(d)
+
+# 数据类型的概念
+# 在python里数据都有各自对应的类型
+
+
+print(45)  # int 整数类型
+print(3.145)  # float 类型
+print((-1) ** 0.5)  # complex类型
+
+# 字符串类型：其实就是一段普通的文字
+# python 里的字符串要求i使用一对单引号，或者双引号来包裹
+print('今天天气好晴朗，处处好好风光')
+print('56')
+
+# 布尔类型：用来表示真假/对错
+# 布尔类型只有两个值，False或True
+print(4 > 3)  # 对
+print(1 > 5)  # 错
+
+# 列表类型
+names = ['James', 'Ken', 'Xiong']
+
+# 字典类型
+person = {'name': 'James', 'age': 24, 'addr': 'hubei'}
+
+# 元组类型
+nums = (1, 2, 3, 4, 5)
+
+# 集合类型
+x = [9, 'hello', True]
+
+
